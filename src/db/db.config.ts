fb26f803f0209eb21166717cb39283082f30f5af
vivek <vivek.malik1705@gmail.com>
2025-02-09 17:38:22 +0530

    commit-00

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -1,12 +1,10 @@
 import { DataSource } from "typeorm";
-import path from "path";
-
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: "postgresql://db0_l1ar_user:AutakAnDPZc0IWJSV2XRpduYNnGeDc0s@dpg-cuk97tlumphs73bbiovg-a.oregon-postgres.render.com/db0_l1ar", 
-    synchronize: false, // Set to false in production, use migrations instead
+    synchronize: false,
     logging: true,
     entities: ["src/entities/*.ts"],
     migrations: ["src/migrations/*.ts"],
-    ssl: { rejectUnauthorized: false } , 
+    ssl: { rejectUnauthorized: false }
 });
